180096150b5b941c222d6693760e60265d05d56d
Erick Gustavo <erick.stazs@gmail.com>
2023-01-10 09:12:15 -0300

    Fix cors error

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ const routes = require('./routes');
 require('express-async-errors');
 
 const app = express();
+const cors = require('./app/middlewares/cors');
+const errorHandler = require('./app/middlewares/errorHandler');
 
 app.use(express.json());
+app.use(cors);
 app.use(routes);
-app.use((error, request, response, next) => {
-  console.log('=== Error Handler ===');
-  console.log(error);
-  response.sendStatus(500);
-});
+app.use(errorHandler);
 
 app.listen(3333, () => console.log('🔥 Server started at http://localhost:3333 🔥'));
